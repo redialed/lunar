c6d866c2d4357d73170145bea7705d709680cd2d
Alessio Di Paolo <87091573+lvuu@users.noreply.github.com>
2024-04-18 23:46:22 +0200

    like list implemented!

--- routes/api/v1/media.js
+++ routes/api/v1/media.js
@@ -4,8 +4,9 @@ const router = express.Router();
 const Post = require("../../../models/Post"); // Assuming your Post model is defined in a separate file
 const User = require("../../../models/User");
 const Like = require("../../../models/Like");
+const Follow = require("../../../models/Follow");
 
-router.post('/configure', async (req, res) => {
+router.post("/configure", async (req, res) => {
   try {
     const { ds_user_id, sessionid } = req.cookies;
 
@@ -18,7 +19,10 @@ router.post('/configure', async (req, res) => {
     }
 
     // Ensure user is authenticated
-    const user = await User.findOne({ userID: ds_user_id, sessionID: sessionid }).exec();
+    const user = await User.findOne({
+      userID: ds_user_id,
+      sessionID: sessionid,
+    }).exec();
     if (!user) {
       return res.status(401).json({
         message: "Unauthorized",
@@ -44,18 +48,20 @@ router.post('/configure', async (req, res) => {
       upload_id = device_timestamp;
     }
 
-    await Post.updateOne({ originalUploadID: upload_id, originalUUID: uuid }, { description: caption });
+    await Post.updateOne(
+      { originalUploadID: upload_id, originalUUID: uuid },
+      { description: caption }
+    );
     await User.updateOne({ userID: ds_user_id }, { $inc: { photoCount: 1 } });
 
-
-    res.json({ status: 'ok' });
+    res.json({ status: "ok" });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Internal Server Error' });
+    res.status(500).json({ message: "Internal Server Error" });
   }
 });
 
-router.post('/:id/like', async (req, res) => {
+router.post("/:id/like", async (req, res) => {
   const { ds_user_id, sessionid } = req.cookies;
 
   if (!ds_user_id || !sessionid) {
@@ -67,7 +73,10 @@ router.post('/:id/like', async (req, res) => {
   }
 
   // Ensure user is authenticated
-  const user = await User.findOne({ userID: ds_user_id, sessionID: sessionid }).exec();
+  const user = await User.findOne({
+    userID: ds_user_id,
+    sessionID: sessionid,
+  }).exec();
   if (!user) {
     return res.status(401).json({
       message: "Unauthorized",
@@ -101,10 +110,10 @@ router.post('/:id/like', async (req, res) => {
 
   await Post.updateOne({ postID: post.postID }, { $inc: { likes: 1 } });
 
-  res.json({ status: 'ok' });
+  res.json({ status: "ok" });
 });
 
-router.post('/:id/unlike', async (req, res) => {
+router.post("/:id/unlike", async (req, res) => {
   const { ds_user_id, sessionid } = req.cookies;
 
   if (!ds_user_id || !sessionid) {
@@ -116,7 +125,10 @@ router.post('/:id/unlike', async (req, res) => {
   }
 
   // Ensure user is authenticated
-  const user = await User.findOne({ userID: ds_user_id, sessionID: sessionid }).exec();
+  const user = await User.findOne({
+    userID: ds_user_id,
+    sessionID: sessionid,
+  }).exec();
   if (!user) {
     return res.status(401).json({
       message: "Unauthorized",
@@ -147,7 +159,99 @@ router.post('/:id/unlike', async (req, res) => {
 
   await Like.deleteOne();
 
-  res.json({ status: 'ok' });
+  res.json({ status: "ok" });
+});
+
+router.get("/:id/likers", async (req, res) => {
+  const { ds_user_id, sessionid } = req.cookies;
+
+  if (!ds_user_id || !sessionid) {
+    return res.status(401).json({
+      message: "Unauthorized",
+      status: "fail",
+      error_type: "authentication",
+    });
+  }
+
+  // Ensure user is authenticated
+  const user = await User.findOne({
+    userID: ds_user_id,
+    sessionID: sessionid,
+  }).exec();
+  if (!user) {
+    return res.status(401).json({
+      message: "Unauthorized",
+      status: "fail",
+      error_type: "authentication",
+    });
+  }
+
+  // Ensure post exists
+  const post = await Post.findOne({ postID: req.params.id }).exec();
+
+  if (!post) {
+    return res.status(404).json({
+      status: "fail",
+      error_type: "post_not_found",
+    });
+  }
+
+  const likers = await Like.find({ to: post.postID }).exec();
+
+  const likersList = [];
+
+  for (const like of likers) {
+    const liker = await User.findOne({ userID: like.from }).exec();
+
+    // check if logged in user is following the liker
+    const following = await Follow.findOne({
+      from: ds_user_id,
+      to: liker.userID,
+    }).exec();
+
+    // check if liker is following the logged in user
+    const followedBy = await Follow.findOne({
+      from: liker.userID,
+      to: ds_user_id,
+    }).exec();
+
+    likersList.push({
+      pk: liker.userID,
+      pk_id: liker.userID,
+      username: liker.username,
+      full_name: liker.fullname,
+      is_private: liker.private,
+      strong_id__: liker.userID,
+      is_verified: liker.verified,
+      profile_pic_id: "3332305783938388991_58387806137",
+      profile_pic_url: liker.profilePicture,
+      account_badges: [],
+      friendship_status: {
+        following: following ? true : false,
+        followed_by: followedBy ? true : false,
+        blocking: false,
+        muting: false,
+        is_private: false,
+        incoming_request: false,
+        outgoing_request: false,
+        is_blocking_reel: false,
+        is_muting_reel: false,
+        is_bestie: false,
+        is_restricted: false,
+        is_feed_favorite: false,
+        subscribed: false,
+        is_eligible_to_subscribe: false,
+        is_muting_notes: false,
+      },
+      latest_reel_media: 0,
+    });
+  }
+
+  res.json({
+    users: likersList,
+    user_count: likersList.length,
+    status: "ok",
+  });
 });
 
 module.exports = router;
